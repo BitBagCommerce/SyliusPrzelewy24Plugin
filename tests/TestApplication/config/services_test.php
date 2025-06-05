cec8939071b4df16873182705cd1f52581b610a1
Norbert Glanc <norglanc@gmail.com>
2025-06-05 14:30:28 +0200

    Add sylius/test-application package

---
+++ tests/TestApplication/config/services_test.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius CMS Plugin package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $container) {
+    $env = $_ENV['APP_ENV'] ?? 'dev';
+
+    if (str_starts_with($env, 'test')) {
+        $container->import('../../../vendor/sylius/sylius/src/Sylius/Behat/Resources/config/services.xml');
+        $container->import('@BitBagSyliusPrzelewy24Plugin/tests/Behat/Resources/services.xml');
+    }
+};
